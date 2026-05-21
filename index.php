1531b4fd2e292e194096124a8af58ae2ad9e30cb
Romlabs <rommelyasay8@gmail.com>
2026-05-22 05:55:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 if (isset($_SESSION['user_id'])) {
     header("Location: dashboard.php");
