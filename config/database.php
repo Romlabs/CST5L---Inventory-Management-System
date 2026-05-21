289b3123885e62f987edf41b11ca3e114adb1bcd
Romlabs <rommelyasay8@gmail.com>
2026-05-22 03:31:07 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,14 @@
 <?php
-$host = 'localhost';
-$dbname = 'inventory_db';
-$username = 'root';
-$password = '';
+// config/database.php
+$host = getenv('MYSQLHOST') ?: 'localhost';
+$dbname = getenv('MYSQLDATABASE') ?: 'inventory_db';
+$username = getenv('MYSQLUSER') ?: 'root';
+$password = getenv('MYSQLPASSWORD') ?: '';
+$port = getenv('MYSQLPORT') ?: '3306';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Note the addition of the port in the DSN string
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
